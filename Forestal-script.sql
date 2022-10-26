978279f1fc47aacf6dd2da1bcdd09bb20e19104d
marian1090 <mariann1042@gmail.com>
2022-10-26 16:48:01 -0300

    Update Forestal-script.sql

--- Forestal-script.sql
+++ Forestal-script.sql
@@ -1,3 +1,19 @@
 CREATE DATABASE forestal
 
---
+
+--tipo_empleado
+--empleado
+--anticipo
+
+--supermercado
+--vale
+--insumos
+
+--empleado-insumo
+--remito
+--empleado-remito
+
+--corte
+--producciones
+--producciones-corte
+--empleado-producciones
