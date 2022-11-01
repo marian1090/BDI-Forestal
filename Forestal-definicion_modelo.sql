912e8c0c8a9437497fcde9f223acbe1d2b13b77f
marian1090 <mariann1042@gmail.com>
2022-11-01 16:00:39 -0300

    Update Forestal-definicion_modelo.sql

--- Forestal-definicion_modelo.sql
+++ Forestal-definicion_modelo.sql
@@ -24,7 +24,7 @@ USE Forestal;
 
 --TABLA TIPO_EMPLEADO
 CREATE TABLE tipo_empleado(
-	id_TipoEmpleado INT IDENTITY(1,1) not null,
+	id_TipoEmpleado INT IDENTITY not null,
 	nombre VARCHAR(50) not null,
 	sueldoBasico DECIMAL(10,2) not null,
 	activo VARCHAR(2) not null,
@@ -33,7 +33,7 @@ CREATE TABLE tipo_empleado(
 
 --TABLA EMPLEADO
 CREATE TABLE empleado(
-	cod_empleado INT IDENTITY(1,1) not null,
+	cod_empleado INT IDENTITY not null,
 	id_TipoEmpleado INT not null,
 	legajo VARCHAR(15) not null,
 	nombre VARCHAR(50) not null,
@@ -47,7 +47,7 @@ CREATE TABLE empleado(
 
 --TABLA ANTICIPO
 CREATE TABLE anticipo(
-	id_anticipo INT not null,
+	id_anticipo INT IDENTITY not null,
 	cod_empleado INT not null,
 	fecha DATE not null,
 	monto DECIMAL(10,2) not null,
@@ -60,7 +60,7 @@ CREATE TABLE anticipo(
 
 --TABLA TIPO VALE
 CREATE TABLE tipo_vale(
-	id_tipo_vale INT IDENTITY(1,1) not null,
+	id_tipo_vale INT IDENTITY not null,
 	nombre VARCHAR(50) not null,
 	direccion VARCHAR(200) not null,
 	CONSTRAINT PK_tipo_vale PRIMARY KEY (id_tipo_vale)
@@ -81,15 +81,15 @@ CREATE TABLE vale(
 
 --TABLA PROVEEDOR
 CREATE TABLE proveedor(
-	id_proveedor INT IDENTITY(1,1) not null,
+	id_proveedor INT IDENTITY not null,
 	nombre VARCHAR(50) not null,
 	direccion VARCHAR(2000) not null,
-	CONSTRAINT PK_proveedor PRIMARY KEY(id_proveedor),
+	CONSTRAINT PK_proveedor PRIMARY KEY(id_proveedor)
 );
 
 --TABLA INSUMO
 CREATE TABLE insumo(
-	id_insumo INT IDENTITY(1,1) not null,
+	id_insumo INT IDENTITY not null,
 	producto VARCHAR(50) not null,
 	precio DECIMAL(10,2) not null,
 	id_proveedor INT not null,
@@ -105,7 +105,7 @@ CREATE TABLE empleado_insumo(
 	fecha DATE not null,
 	periodo VARCHAR(7) not null,
 	cantidad int not null,
-	CONSTRAINT PK_empleado_insumo PRIMARY KEY(cod_empleado, id_insumo, fecha),
+	CONSTRAINT PK_empleado_insumo PRIMARY KEY(id_insumo, cod_empleado, fecha),
 	CONSTRAINT FK_empleado_insumo_empleado FOREIGN KEY (cod_empleado) REFERENCES empleado(cod_empleado),
 	CONSTRAINT FK_empleado_insumo_insumo FOREIGN KEY (id_insumo) REFERENCES insumo(id_insumo),
 );
@@ -113,7 +113,7 @@ CREATE TABLE empleado_insumo(
 
 --TABLA CORTE
 CREATE TABLE corte(
-	cod_corte INT IDENTITY(1,1) not null ,
+	cod_corte INT IDENTITY not null ,
 	corte VARCHAR(50) not null,
 	descripcion VARCHAR(200) not null,
 	precioVenta DECIMAL(10,2) not null,
@@ -122,9 +122,9 @@ CREATE TABLE corte(
 );
 --TABLA PRODUCCION
 CREATE TABLE produccion(
-	id_produccion INT IDENTITY(1,1),
-	cod_corte INT not null,
+	id_produccion INT IDENTITY,
 	fecha DATE not null,
+	toneladas DECIMAL(10,2) not null,
 	CONSTRAINT PK_produccion PRIMARY KEY(id_produccion),
 );
 
@@ -149,7 +149,7 @@ CREATE TABLE empleado_produccion(
 --TABLA REMITO
 CREATE TABLE remito(
 	primeros INT not null,
-	id_remito INT not null,
+	id_remito INT IDENTITY not null,
 	id_produccion INT not null,
 	fecha DATE not null,
 	cantidad INT not null,
