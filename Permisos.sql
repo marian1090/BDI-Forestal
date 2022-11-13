827454aed6fd3ebe385c0c52b400396969689bab
marian1090 <mariann1042@gmail.com>
2022-11-13 18:19:39 -0300

    Create Permisos.sql

---
+++ Permisos.sql
@@ -0,0 +1,52 @@
+﻿--------------------------------------
+--BASE DE DATOS I.
+--PROYECTO: BD EMPRESA FORESTAL 
+--GRUPO 20 
+--COMISION 4
+--INTEGRANTES:
+--          BARTOLUCCI, Gast�n Leonel
+--          OJEDA, Lidia Mariangeles	
+--          RAMIREZ, Alfredo Agust�n	
+--          ZAPATA, Sergio Ariel
+--PERMISOS
+--CREACION DE USUARIOS Y ASIGNACION DE ROLES.
+--------------------------------------.
+
+--CREACION  DE INICIO DE SESION ADMINISTRADOR y ROL SYSADMIN--
+CREATE LOGIN [Administrador] WITH PASSWORD = '1234',
+CHECK_EXPIRATION = OFF, 
+CHECK_POLICY = OFF;
+CREATE USER Gerente FOR LOGIN Administrador;
+EXEC sys.sp_addsrvrolemember @loginame = N'Administrador', @rolename = N'sysadmin';
+
+
+
+--CREACION  DE INICIO DE SESION ABM Y ROL DE LECTURA/ESCRITURA--
+CREATE LOGIN [ABM] WITH PASSWORD = '1234',
+CHECK_EXPIRATION = OFF, 
+CHECK_POLICY = OFF,
+DEFAULT_DATABASE = Forestal;
+GO;
+USE Foretal;
+CREATE USER Administrativo FOR LOGIN ABM;
+EXEC sp_addrolemember 'db_datareader','Rodrigo';
+EXEC sp_addrolemember 'db_datawriter','Rodrigo';
+GO;
+
+
+--CREACION  DE INICIO DE SESION CONSULTA Y ROL PUBLICO--
+CREATE LOGIN [Consulta] WITH PASSWORD = '1234',
+CHECK_EXPIRATION = OFF, 
+CHECK_POLICY = OFF,
+DEFAULT_DATABASE = base_inventario;
+USE base_inventario
+CREATE USER Empleado FOR LOGIN Consulta;
+-- ASIGNACION DE FUNCIONES DE USUARIO
+--LOGIN: CONSULTA
+GRANT select on VW_INSUMOS_UTILIZADOS to Empleado
+GRANT select on VW_NOMINA_EMPLEADOS to Empleado
+GRANT select on VW_REMITO_PRODUCCION to Empleado
+GRANT select on VW_VALES_UTILIZADOS to Empleado
+Deny Insert, Delete, Update to Empleado
+
+
