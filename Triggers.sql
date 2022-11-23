a62c7131e4543f97a3dcae688c4fb522239836fc
marian1090 <mariann1042@gmail.com>
2022-11-23 15:00:56 -0300

    Update Triggers.sql

--- Triggers.sql
+++ Triggers.sql
@@ -15,7 +15,7 @@ CREATE TRIGGER TR_empleado ON empleado
 FOR INSERT
 AS 
 PRINT 'Empleado Registrado'
-INSERT INTO empleado (id_TipoEmpleado, legajo, nombre, apellido, cuil, fecha_ingreso, activo) VALUES(7,'13/0','ANDREZ','ESTEPA',	20149737856	,'10/01/1996','SI');
+
 
 --Cambia el activo a 'NO' de un empleado cuando el tipo de empleado asociado cambia a 'NO'
 CREATE TRIGGER TR_Tipoempleado ON tipo_empleado FOR UPDATE
@@ -24,17 +24,6 @@ DECLARE @id int = (SELECT id_TipoEmpleado FROM inserted)
 UPDATE empleado SET activo = (SELECT activo FROM tipo_empleado 
 								WHERE id_TipoEmpleado = @id)
 WHERE id_TipoEmpleado = @id
-
-UPDATE tipo_empleado SET activo = 'NO' WHERE id_TipoEmpleado = 8
-
-
---Cambia los primeros de la tabla empleado_remito cuando se modifican los primeros en la tabla remito
-CREATE TRIGGER TR_rem ON remito FOR UPDATE
-AS DECLARE @id int = (SELECT id_remito FROM inserted)
-UPDATE empleado_remito SET primeros = 
-									(SELECT primeros FROM remito
-									WHERE id_remito = @id)
-WHERE id_remito = @id
 
 --Borra de la tabla empleado_produccion el registro asociado a una produccion cancelada
 CREATE TRIGGER TR_empleado_produccion ON produccion
