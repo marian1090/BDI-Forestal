c714da46cbac0c693c152dbba41122ba39455054
agustinn35 <ramirezalfredoagustin80@gmail.com>
2022-11-23 11:24:19 -0300

    Update Funciones.sql

--- Funciones.sql
+++ Funciones.sql
@@ -143,3 +143,53 @@ END
 SELECT e.cod_empleado,e.nombre, dbo.F_SueldoEmpleado(4,10) 
 FROM empleado e 
 WHERE e.cod_empleado = 4
+
+CREATE FUNCTION  F_SueldoEmpleado_Tabla (
+@empleado INT,
+@fecha INT
+)
+RETURNS TABLE
+AS 
+	RETURN(SELECT e.cod_empleado,CONCAT(e.nombre,' ',e.apellido) 'Empleado', (te.sueldoBasico) + (ISNULL(TotalRemitoEmpleado.Total,0)) - (ISNULL(InsumoEmpleado.Total,0)) - (ISNULL(Vale.Mvale,0)) - (ISNULL(Anticipo.MAnticipo,0)) 'Sueldo'
+
+FROM    empleado e
+INNER JOIN tipo_empleado te ON e.id_TipoEmpleado = te.id_TipoEmpleado
+FULL OUTER JOIN 
+(                                                  --calculo del monto de los remitos entregados por empleado para el mes 10
+	SELECT er.cod_empleado, CAST(SUM((c.precioVenta * pc.cantidad) * (r.cantidad/p.toneladas)) AS DECIMAL(10,2)) 'Total'
+		FROM produccion_corte pc
+		INNER JOIN corte c ON (pc.cod_corte = c.cod_corte)
+		INNER JOIN remito r ON (pc.id_produccion = r.id_produccion)
+		INNER JOIN produccion p ON (pc.id_produccion = p.id_produccion)
+		INNER JOIN empleado_remito er ON r.id_remito = er.id_remito
+		WHERE CAST(DATEPART(MONTH,r.fecha)AS INTEGER) = @fecha 
+		GROUP BY er.cod_empleado
+)AS TotalRemitoEmpleado ON e.cod_empleado = TotalRemitoEmpleado.cod_empleado
+FULL OUTER JOIN
+(                                                 --calculo de los insumos utilzados por los empleados para el mes 10
+	SELECT SUM(i.precio * ei.cantidad) 'Total', ei.cod_empleado
+		FROM empleado_insumo ei
+		INNER JOIN insumo i ON (ei.id_insumo = i.id_insumo )
+		WHERE CAST(DATEPART(MONTH,ei.fecha)AS INTEGER) = @fecha
+		GROUP BY ei.cod_empleado 
+) AS InsumoEmpleado ON e.cod_empleado = InsumoEmpleado.cod_empleado
+FULL OUTER JOIN
+(                                                 --calculo de los vales utilizados 
+	SELECT SUM(v.monto) Mvale, e.cod_empleado
+		FROM empleado e
+		FULL OUTER JOIN vale v ON (e.cod_empleado = v.cod_empleado)
+		WHERE  CAST(DATEPART(MONTH,v.fecha)AS INTEGER) = @fecha 
+		GROUP BY e.cod_empleado
+) AS Vale ON e.cod_empleado = vale.cod_empleado
+FULL OUTER JOIN
+(                                                 --calcilo de los anticipos solicitados del mes 10
+	SELECT SUM(a.monto) MAnticipo, e.cod_empleado
+		FROM empleado e
+		FULL OUTER JOIN anticipo a ON (e.cod_empleado = a.cod_empleado)
+		WHERE CAST(DATEPART(MONTH,a.fecha)AS INTEGER) = @fecha 
+		GROUP BY e.cod_empleado
+) AS Anticipo ON e.cod_empleado = Anticipo.cod_empleado
+WHERE e.cod_empleado = @empleado
+GROUP BY e.cod_empleado,CONCAT(e.nombre,' ',e.apellido),te.sueldoBasico,TotalRemitoEmpleado.Total,InsumoEmpleado.Total,Vale.Mvale,Anticipo.MAnticipo)
+
+SELECT * FROM dbo.F_SueldoEmpleado_Tabla(2,10)
